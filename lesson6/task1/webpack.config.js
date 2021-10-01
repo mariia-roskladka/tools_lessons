175cb63f82b6423b3b96fc4886efaf37a27fd593
mariia <rmashka2001@gmail.com>
2021-10-01 14:38:54 +0300

    6.1

---
+++ lesson6/task1/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+                test: /\.s?css$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        outputPath: 'images',
+                    },
+                }, ],
+            },
+        ],
+    },
+};
